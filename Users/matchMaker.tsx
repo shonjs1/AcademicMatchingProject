8020d9762d6e9239f41f4f6ab1aa0fafbd99c483
ChanceNason <cnason349@gmail.com>
2023-09-17 17:00:38 -0400

    Starting point of match maker

---
+++ Users/matchMaker.tsx
@@ -0,0 +1,13 @@
+const matchMaker = require('./Users/user')
+
+async function matchUsers() {
+    const users = "GET request to fetch all users";
+
+    const matchedPairs = [];
+
+    for (let i = 0; i < users.length; i++) {
+        for (let j = i + 1; j < users.length; j++) {
+            
+        }
+    }
+}
