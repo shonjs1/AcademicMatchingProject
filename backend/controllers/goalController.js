7b086afd9c62a9a69fa6c5b1025d8c0e3bbc3fac
shonjs <jihyukshon@gmail.com>
2023-09-17 16:48:09 -0400

    Added to GET and POST methods to successfully interact with DB

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,17 +1,30 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
+
 // @desc Get goals 
 // @route GET / api/goals 
 // @access Private 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc Set goals 
 // @route POST / api/goals 
 // @access Private 
 const setGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Set goals'})
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+
+    const goal = await Goal.create({ 
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 // @desc Update goals 
