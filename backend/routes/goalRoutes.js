20511c249698789ae3dc7ed36902e8b4616f4b3b
shonjs <jihyukshon@gmail.com>
2023-09-17 14:38:11 -0400

    Added controllers for POST, PUT, DELETE method

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,15 @@
 //each resource in API will have own route file.
 const express = require('express')
 const router = express.Router()
-const { getGoals } = require('../controllers/goalController') 
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController') 
+
 router.get('/', getGoals)
 
-router.post('/', (req, res) =>{ //Create goal
-    res.status(200).json({message: 'Set goals'})
-})
+router.post('/', setGoal)
 
-router.put('/:id', (req, res) =>{ //needs ID**
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-})
+router.put('/:id', updateGoal)
 
-router.delete('/:id', (req, res) =>{ //needs ID**
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
+router.delete('/:id', deleteGoal)
 
 
 module.exports = router
