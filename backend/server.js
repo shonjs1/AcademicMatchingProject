d8efd7d86f919d35e123f1526d7c74095a0f5a50
shonjs <jihyukshon@gmail.com>
2023-09-17 13:59:32 -0400

    Server up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+const express = require('express') //bring express in
+const dotenv = require('dotenv').config() //have environment variables 
+const port = process.env.PORT || 5000
+
+const app = express() //initialize express
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
