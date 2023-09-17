010648aff4f8ad8890d17d540007a312ea7091dc
shonjs <jihyukshon@gmail.com>
2023-09-17 16:04:33 -0400

    Added DB connection code || Successfully able to connect to our MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express') //bring express in
+const colors = require('colors') 
 const dotenv = require('dotenv').config() //have environment variables 
 const port = process.env.PORT || 5000
-
+const connectDB = require('./config/db')
 const app = express() //initialize express
 
+connectDB()
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
