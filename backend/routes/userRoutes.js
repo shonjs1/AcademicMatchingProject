d32fce6bc02baedb23745917a5b02654c66f4ac0
ChanceNason <cnason349@gmail.com>
2023-09-19 18:42:47 -0400

    More bug fixing...

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { getUsers, setUser, updateUser, deleteUser } = require('../controllers/userController')
 
-router.route('./').get(getUsers).post(setUser)
+router.route('/').get(getUsers).post(setUser)
 // router.get('/', getUsers)
-router.post('./', setUser)
+router.post('/', setUser)
 
-router.route('./:id').put(updateUser).delete(deleteUser)
-// router.put('./', updateUser)
-router.delete('./', deleteUser)
+router.route('/:id').put(updateUser).delete(deleteUser)
+// router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 module.exports = router
