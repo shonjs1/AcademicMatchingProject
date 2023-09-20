47f9d20a43d1981f2db0369e391f02aa5d9a8a0a
shonjs <jihyukshon@gmail.com>
2023-09-20 19:56:47 -0400

    Working on controller for registering user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,33 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc Register New User 
 // @route POST / api/users 
 // @access Public 
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body 
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
     res.json({message: 'Register User'})
-}
+})
 
 // @desc Authenticate a User 
 // @route POST / api/users/login
 // @access Public 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({message: 'Login User'})
-}
+})
 
 // @desc Get user data
 // @route GET / api/users/me
 // @access Public 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({message: 'Display User Info'})
-}
+})
 
 
 module.exports = {
