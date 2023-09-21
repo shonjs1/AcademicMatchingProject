08db0f00160d777225d7e915869b3cf8ecda5d1b
ChanceNason <cnason349@gmail.com>
2023-09-20 20:57:32 -0400

    Fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://Test:1111@cluster0.icutero.mongodb.net/mernapp")
+        const conn = await mongoose.connect(process.env.MONGO_URI)
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline );
     } catch (error) {
