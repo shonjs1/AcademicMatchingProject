5f2a8820f35feb3fa5182c5e27f16cfc531a20d0
shonjs <jihyukshon@gmail.com>
2023-09-23 15:21:27 -0400

    added functionality for User Registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,37 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add all fields')
     }
-    res.json({message: 'Register User'})
+
+    //Checks user existence 
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
 })
 
 // @desc Authenticate a User 
