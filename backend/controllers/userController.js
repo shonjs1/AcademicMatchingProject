7572c6a7c08ab425a9f9f2fe1109fa4ad34265b2
shonjs <jihyukshon@gmail.com>
2023-09-23 16:24:37 -0400

    Created middleware to authenticate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,7 +36,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else{
         res.status(400)
@@ -49,15 +50,44 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST / api/users/login
 // @access Public 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+    // check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+    res.json( { 
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid Credentials')
+    }
+    res.json({ message: 'Login User' })
 })
 
 // @desc Get user data
 // @route GET / api/users/me
-// @access Public 
+// @access Private 
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: 'Display User Info'})
+    const {_id, name, email} = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name, 
+        email,
+    })
 })
+
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 
 module.exports = {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser, getMe } = require('../controllers/userController')
+
+const {protect} = require('../middleware/authMiddleware')
 //adding a user 
 router.post('/', registerUser)
 //authenticate
 router.post('/login', loginUser)
 //get user info
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 
 
