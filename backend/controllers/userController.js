f4a482768c3e77aff9b8b49b703c521c4eda0159
ChanceNason <cnason349@gmail.com>
2023-09-23 17:22:36 -0400

    getUsers test

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,9 @@ const User = require('../models/userModel')
 
 
 const getUsers = asyncHandler(async (req, res) => {
-    const users = await User.find()
+    const users = await User.find({})
     res.status(200).json(users)
-})
+});
 
 const setUser = asyncHandler(async (req, res) => {
     try {
