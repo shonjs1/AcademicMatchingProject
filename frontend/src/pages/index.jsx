8a3c614470bc38c2ed188753e5080987a25dccdd
A-Zayed <ayzayed1@gmail.com>
2023-10-24 21:20:26 -0500

    added match button to profile page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,14 +1,39 @@
+// import React from 'react';
+// import '../stylesheet/index.css';
+// const profile = process.env.PUBLIC_URL + '/images/books2.jpg';
+
+// export default function Home() {
+//     return <div className="container">
+//         <div className="motto"><h1>Tutoring Initiative Network for Diligent Education and Resources</h1>
+//         </div>
+//         <div className="background">
+
+//         </div>
+
+//         </div>;
+// }
+
 import React from 'react';
 import '../stylesheet/index.css';
-// import books2 from "./images/books2.jpeg";
+const profile = process.env.PUBLIC_URL + '/images/books2.jpg';
 
 export default function Home() {
-    return <div className="container">
-        <div className="motto"><h1>Tutoring Initiative Network for Diligent Education and Resources</h1>
-        </div>
-        <div className="background">
-            
-        </div>
+    const backgroundStyle = {
+        backgroundImage: `url(${profile})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        height: '100vh', // Adjust this value as needed
+    };
 
-        </div>;
+    return (
+        <div className="container">
+            <div className="motto">
+                <h1>Tutoring Initiative Network for Diligent Education and Resources</h1>
+            </div>
+            <div className="background" style={backgroundStyle}>
+                {/* Content for the background */}
+            </div>
+        </div>
+    );
 }
