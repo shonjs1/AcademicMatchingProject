9712fc0c2554fea72f10c25b12d3b064a58fc0ef
ChanceNason <cnason349@gmail.com>
2023-10-26 17:21:05 -0400

    Do not need

--- frontend/src/pages/event-utils.js
+++ frontend/src/pages/event-utils.js
@@ -1,19 +1,3 @@
-let eventGuid = 0
-let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of today
+let eventGuid = 0;
 
-export const INITIAL_EVENTS = [
-    {
-    id: createEventId(),
-    title: 'First Group Meeting',
-    start: todayStr
-    },
-    {
-    id: createEventId(),
-    title: 'Meet Time',
-    start: todayStr + 'T12:00:00'
-    }
-]
-
-export function createEventId() {
-    return String(eventGuid++)
-}
+export const createEventId = () => String(eventGuid++);
